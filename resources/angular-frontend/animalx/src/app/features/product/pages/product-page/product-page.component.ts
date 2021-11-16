f393ae2f0d233944613e26b922d8a99f58a353ad
alxiim <jimvanijcken@hotmail.com>
2021-11-16 22:41:45 +0100

    fix cart

--- resources/angular-frontend/animalx/src/app/features/product/pages/product-page/product-page.component.ts
+++ resources/angular-frontend/animalx/src/app/features/product/pages/product-page/product-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartService } from 'src/app/core/http/cart/cart.service';
 import { Product } from 'src/app/core/models/product.model';
 
 @Component({
@@ -12,7 +13,8 @@ export class ProductPageComponent implements OnInit {
     product: Product;
 
     constructor(
-        private _route: ActivatedRoute
+        private _route: ActivatedRoute,
+        private _cartService: CartService
     ) {
         // TODO should this be in OnInit?
         this.product = this._route.snapshot.data.product;
@@ -22,7 +24,7 @@ export class ProductPageComponent implements OnInit {
     }
 
     addToCart() {
-        // TODO add to cart
+        this._cartService.update(this.product.id, 1);
     }
 
 }
