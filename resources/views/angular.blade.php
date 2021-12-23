323bbc543cc64f39f6b7f5c0a4d367e7ea95168d
alxiim <jimvanijcken@hotmail.com>
2021-12-23 13:56:57 +0100

    fix category images

--- resources/views/angular.blade.php
+++ resources/views/angular.blade.php
@@ -12,7 +12,7 @@
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script src="runtime.c40e428e57e52ce18a96.js" defer></script><script src="polyfills.52abfa2fd0f7f100a353.js" defer></script><script src="main.f2b178b0618c32188ff8.js" defer></script>
+    <script src="runtime.f02b0d0474ddd8a283a5.js" defer></script><script src="polyfills.52abfa2fd0f7f100a353.js" defer></script><script src="main.f2b178b0618c32188ff8.js" defer></script>
 
 
 </body></html>
