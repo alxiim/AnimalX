0106c653963a4440e065eb787208a43ef776ba10
alxiim <jimvanijcken@hotmail.com>
2021-12-25 10:00:38 +0100

    orders routing

--- resources/angular-frontend/animalx/src/app/features/account/account-routing.module.ts
+++ resources/angular-frontend/animalx/src/app/features/account/account-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccountPageComponent } from './pages/account-page/account-page.component';
+import { OrdersPageComponent } from './pages/orders-page/orders-page.component';
 
 const routes: Routes = [{
     path: '',
     component: AccountPageComponent
+}, {
+    path: 'orders',
+    component: OrdersPageComponent
 }];
 
 @NgModule({
