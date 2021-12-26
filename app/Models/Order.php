578ebb126ed296d5e88b973f32ff3b9bc28d0114
alxiim <jimvanijcken@hotmail.com>
2021-12-26 11:54:24 +0100

    images

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -31,6 +31,6 @@ class Order extends Model
      */
     public function products()
     {
-        return $this->belongsToMany(Product::class)->withPivot('amount');
+        return $this->belongsToMany(Product::class)->with('images')->withPivot('amount');
     }
 }
